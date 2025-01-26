84e25cb4db919c64c5fd4c388b161b5408c8755f
Marc Sallent <marc@wechoosefun.com>
2025-01-26 19:56:34 +0100

    Gives access to the PointCloud

--- src/ofxLibRealSense2P.h
+++ src/ofxLibRealSense2P.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <librealsense2/rs.hpp> 
+#include <librealsense2/rs.hpp>
 #include <librealsense2/rsutil.h>
 #include "ofMain.h"
 #include "ofxLRS2/Filter.h"
@@ -50,7 +50,7 @@ public:
 	void enableIr(int width, int height, int fps = 60, bool useArbTex = true);
 	void enableDepth(int width, int height, int fps = 60, bool useArbTex = true);
 	
-	//record 
+	//record
 	void startRecord(string path);
 	void stopRecord();
 	void playbackRecorded();
@@ -73,14 +73,14 @@ public:
 			ofLogWarning() << "getDurationAs() : it can be used in playbacking bag only.";
 		}
 	}
-
-
+	
+	
 	void threadedFunction();
 	void update();
-
+	
 	float getDistanceAt(int x, int y);
 	glm::vec3 getWorldCoordinateAt(float x, float y);
-
+	
 	//make frames size aligned
 	void setAligned(bool aligned)
 	{
@@ -93,107 +93,131 @@ public:
 			ofLogWarning() << "Align FAILED";
 		}
 	}
-
+	
 	void drawDepthRaw(int x, int y)
 	{
 		if (_raw_depth_frame)
 			_raw_depth_frame->drawTexture(x, y);
 	}
-
+	
 	void drawDepth(int x, int y)
 	{
 		if(_depth_frame)
 			_depth_frame->drawTexture(x, y);
 	}
-
+	
 	void drawColor(int x, int y)
 	{
 		if (_color_frame)
 			_color_frame->drawTexture(x, y);
 	}
-
+	
 	void drawIR(int x, int y)
 	{
 		if (_ir_frame)
 			_ir_frame->drawTexture(x, y);
 	}
-
+	
 	float getDepthWidth() const
 	{
 		return _depth_frame->getWidth();
 	}
-
+	
 	float getDepthHeight() const
 	{
 		return _depth_frame->getHeight();
 	}
-
+	
 	float getColorWidth() const
 	{
 		return _color_frame->getWidth();//if it is aligned, then it would not the same value as its originals
 	}
-
+	
 	float getColorHeight() const
 	{
 		return _color_frame->getHeight();//if it is aligned, then it would not the same value as its originals
 	}
-
+	
 	bool isFrameNew()
 	{
 		return bFrameNew;
 	}
-
+	
 	shared_ptr<ofTexture> getDepthTexture() const
 	{
 		return _depth_frame->getTexture();
 	}
-
+	
 	shared_ptr<ofTexture> getDepthRawTexture() const
 	{
 		return _raw_depth_frame->getTexture();
 	}
-
+	
 	shared_ptr<ofTexture> getColorTexture()
 	{
 		return _color_frame->getTexture();
 	}
-
+	
 	float getDepthScale()
 	{
 		return depthScale;
 	}
-
+	
 	void setDepthColorSchema(COLOR_SCHEMA schema)
 	{
 		rs2colorizer.set_option(RS2_OPTION_COLOR_SCHEME, schema);
 	}
-
+	
 	rs2_intrinsics getCameraIntrinsics() const
 	{
 		return intr;
 	}
-
+	
 	string getRecordedFilePath()
 	{
 		return _recordFilePath;
 	}
-
-private:
+	
+	const std::vector<glm::vec3>& getPointCloud() {
+		static std::vector<glm::vec3> points;
+		
+		if (!_depth) return points;
+		
+		// Generate point cloud
+		_points = _pc.calculate(_depth);
+		
+		// Get vertices
+		auto vertices = _points.get_vertices();
+		auto size = _points.size();
+		
+		// Resize vector if needed
+		points.resize(size);
+		
+		// Copy points - vertices is a contiguous array of xyz floats
+		for (size_t i = 0; i < size; i++) {
+			const auto& v = vertices[i];
+			points[i] = glm::vec3(v.x, v.y, v.z);
+		}
+		
+		return points;
+	}
+	
+	private:
 	rs2::device		rs2device;
-	Rs2Config		rs2config; 
+	Rs2Config		rs2config;
 	rs2::colorizer  rs2colorizer;
 	shared_ptr<rs2::pipeline>	rs2_pipeline;
-
+	
 	rs2::frame_queue rs2depth_queue;
 	//rs2::frame_queue rs2video_queue;
 	//rs2::frame_queue rs2ir_queue;
-
+	
 	rs2::frame _depth;
 	rs2_intrinsics intr;
-
+	
 	shared_ptr<BaseFrameUnit<>> _color_frame, _ir_frame, _depth_frame;
 	shared_ptr<BaseFrameUnit<unsigned short>> _raw_depth_frame;
-
+	
 	bool _isRecording;
 	vector<shared_ptr<ofxlibrealsense2p::Filter>> filters;
 	float depthScale;
@@ -212,17 +236,20 @@ private:
 	void process();
 	bool _useThread = true;
 	double _seekingPosition = -1;
-
+	
+	rs2::pointcloud _pc;
+	rs2::points _points;
+	
 	void start()
 	{
 		startThread();
 	}
-
+	
 	void stop()
 	{
 		stopThread();
 	}
-
+	
 	float calcDepthScale(rs2::device dev)
 	{
 		// Go over the device's sensors
@@ -236,7 +263,7 @@ private:
 		}
 		throw std::runtime_error("Device does not have a depth sensor");
 	}
-
+	
 	void updateDepthFrameForPostProcess(rs2::depth_frame& depthFrame)
 	{
 		bool revert_disparity = false;
